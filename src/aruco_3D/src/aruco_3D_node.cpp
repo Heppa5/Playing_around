80edf577101af72f28326540464e4ff8973c8138
rovi2 <jesper.poder@live.dk>
2017-10-27 16:27:35 +0200

    Getting 3D-positions out as topic with 7 hz. Is using timestamps to determine matching 3D-points for robot and camera. Next point is to try and determine a transformation matrix

--- src/aruco_3D/src/aruco_3D_node.cpp
+++ src/aruco_3D/src/aruco_3D_node.cpp
@@ -2,14 +2,29 @@
 
 #include <ros/ros.h>
 #include "std_msgs/String.h"
+#include "std_msgs/Header.h"
+#include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Pose.h"
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/aruco.hpp>
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 using namespace std;
+using namespace cv;
+
+struct cameraPoint {
+  Mat point3D;
+  ros::Time stamp;
+} ;
+
+struct matchedPoint {
+  cameraPoint camera;
+  geometry_msgs::PoseStamped robot;
+} ;
 
 class ImageConverter
 {
@@ -28,6 +43,30 @@ public:
         image_sub_ = it_.subscribe("/camera/image_raw", 1,&ImageConverter::imageCb, this);
         image_pub_ = it_.advertise("/image_converter/output_video", 1);
         robot_sub_ = nh_.subscribe("/robot/moved", 1, &ImageConverter::robot_has_moved,this);
+        
+        // For using solvePNP on aruco Marker
+//         marker_3D.push_back(Point3f(0,0,0));
+//         marker_3D.push_back(Point3f(0.169,0,0));
+//         marker_3D.push_back(Point3f(0.169,-0.169,0));
+//         marker_3D.push_back(Point3f(0,-0.169,0));
+        marker_3D.push_back(Point3f(0,0,0));
+        marker_3D.push_back(Point3f(0.044,0,0));
+        marker_3D.push_back(Point3f(0.044,-0.044,0));
+        marker_3D.push_back(Point3f(0,-0.044,0));
+        
+        
+        distortion.push_back(-0.228385);
+        distortion.push_back(0.266082);
+        distortion.push_back(-0.001812);
+        distortion.push_back(0.000035);
+        distortion.push_back(0.000000);
+        
+        mark_dict = aruco::getPredefinedDictionary(aruco::DICT_4X4_50);
+        
+        camera_matrix = (Mat_<float>(3,3) <<    1348.715676, 0.000000, 722.486120,
+                                                0.000000, 1347.386029, 495.012476,
+                                                0.000000, 0.000000, 1.000000);
+        
     }
 
     ~ImageConverter()
@@ -47,26 +86,140 @@ public:
             ROS_ERROR("cv_bridge exception: %s", e.what());
             return;
         }
-
-
-        // Output modified video stream
-        image_pub_.publish(cv_ptr->toImageMsg());
-
-        current_image = cv_ptr->image.clone();
+        
+        vector< vector<Point2f> > markerCorners; // So, the first corner is the top left corner, followed by the top right, bottom right and bottom left.
+        vector<int> markerIds;
+        aruco::detectMarkers(cv_ptr->image, mark_dict, markerCorners, markerIds);
+        if(!markerCorners.empty())
+        {
+            Mat rvec, tvec;
+            solvePnP(marker_3D,markerCorners[0],camera_matrix,distortion,rvec,tvec);
+            cameraPoint current;
+            current.stamp=msg->header.stamp;
+            current.point3D=tvec.clone();
+            camera_points.insert(camera_points.begin(),current);
+            if(camera_points.size() > 4) // only one second of data is stored
+            {
+                camera_points.pop_back();
+            }
+        }  
+        match_points();
     }
-    void robot_has_moved(const geometry_msgs::Pose::ConstPtr& msg)
+    void robot_has_moved(const geometry_msgs::PoseStamped::ConstPtr msg)
     {
-        ROS_INFO("We are brilliant");
-        cout << *msg << endl;
+//         ROS_INFO("We are brilliant");
+//         cout << *msg << endl;
+//         
+//         ROS_INFO("We are brilliant2");
+        robot_positions.insert(robot_positions.begin(),*msg);
+        if(robot_positions.size() > 2) // only one second of data is stored
+        {
+            robot_positions.pop_back();
+        }
+        match_points();
     }
+    
+    void match_points()
+    {
+//         ROS_INFO("MATCHING POINTS");
+        for( int i = 0 ; i < robot_positions.size() ; i++)
+        {
+            for( int j = 0 ; j < camera_points.size() ; j++)
+            {
+                ros::Duration possible_match(0.0714); // 7 Hz / 2 
+                ros::Time low_bound = camera_points[j].stamp - possible_match;
+                ros::Time high_bound = camera_points[j].stamp + possible_match;
+                if(robot_positions[i].header.stamp>low_bound && robot_positions[i].header.stamp<high_bound)
+                {
+//                     cout << camera_points[j].stamp << " ~ " << robot_positions[i].header.stamp << endl;
+//                     cout << robot_positions[i].pose << endl;
+//                     cout << camera_points[j].point3D << endl;
+//                     ROS_INFO("In a match");
+                    matchedPoint current;
+                    current.camera=camera_points[j];
+                    current.robot=robot_positions[i];
+                    matchedPoints.insert(matchedPoints.begin(),current);
+//                     /*camera_points.erase(camera_points.begin()+j);
+//                     robot_positions.erase(robot_positions.begin()+i)*/;
+                    
+//                     if(matchedPoints.size() > 15) // Allowing more data to be stored here
+//                     {
+//                         matchedPoints.pop_back();
+//                     }
+                }
+            }
+        }
+        if(matchedPoints.size()>0)
+            choosePoints();
+        
+    }
+    
+    void choosePoints()
+    {
+        // if no points has been choosen, then just take the first matched point
+        if(chosenMatchedPoints.size() == 0)
+        {
+            chosenMatchedPoints.insert(chosenMatchedPoints.begin(),matchedPoints[0]);
+            matchedPoints.erase(matchedPoints.begin());
+        }
+//         ROS_INFO("RUNNING CODE AGAIN %d", matchedPoints.size());
+        // choosing points based on, whether the robot has moved position 
+//         if(matchedPoints.size()>0)
+//         {
+            while(matchedPoints.size()>0)
+            {
+//                 ROS_INFO("Hej %f",dist_between_points(matchedPoints[0].robot.pose,chosenMatchedPoints[0].robot.pose));
+//                 ROS_INFO("Wow wow wo");
+                if(compare_3D_points(matchedPoints[0].robot.pose,chosenMatchedPoints[0].robot.pose,0.05))
+                {
+                    ROS_INFO("Hej %d",dist_between_points(matchedPoints[0].robot.pose,chosenMatchedPoints[0].robot.pose));
+                    chosenMatchedPoints.insert(chosenMatchedPoints.begin(),matchedPoints[0]);
+    //                 cout << "hej" << endl;
+                    
+                    matchedPoints.erase(matchedPoints.begin());
+
+                }
+                else
+                    matchedPoints.erase(matchedPoints.begin());
+            }
+//         }
+    }
+    
+    bool compare_3D_points(geometry_msgs::Pose new_position, geometry_msgs::Pose old_position, double expected_distance)
+    {
+        double old_dist=sqrt(pow(old_position.position.x,2)+pow(old_position.position.y,2)+pow(old_position.position.z,2));
+        double new_dist=sqrt(pow(new_position.position.x,2)+pow(new_position.position.y,2)+pow(new_position.position.z,2));
+        
+        if(abs(new_dist-old_dist)>=expected_distance)
+            return true;
+        else 
+            return false;
+    }
+    double dist_between_points(geometry_msgs::Pose new_position, geometry_msgs::Pose old_position)
+    {
+        double old_dist=sqrt(pow(old_position.position.x,2)+pow(old_position.position.y,2)+pow(old_position.position.z,2));
+        double new_dist=sqrt(pow(new_position.position.x,2)+pow(new_position.position.y,2)+pow(new_position.position.z,2));
+        return abs(new_dist-old_dist);
+    }
+    
 private:
     cv::Mat current_image;
+    vector<Point3f> marker_3D;
+    vector<double> distortion;
+    Ptr<aruco::Dictionary> mark_dict;
+    Mat_<float> camera_matrix;
+    vector<cameraPoint> camera_points;
+    vector<geometry_msgs::PoseStamped> robot_positions;
+    vector<matchedPoint> matchedPoints;
+    vector<matchedPoint> chosenMatchedPoints;
+    
 };
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "image_converter");
   ImageConverter ic;
+  ROS_INFO("Starting camera node up");
   ros::spin();
   return 0;
 }
--- src/mp_mini_picker/src/main.cpp
+++ src/mp_mini_picker/src/main.cpp
@@ -25,7 +25,7 @@
 #include <std_msgs/String.h>
 
 
-#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
 
 using namespace ros;
 using namespace std;
@@ -38,9 +38,8 @@ class UrTest
             initWorkCell();
             initDevice();
             initPathPlannerWithCollisionDetector();
-            ROS_INFO("I'm Here");
-            robot_move = nodehandle_.advertise<geometry_msgs::Pose>("/robot/moved", 1);
-            ROS_INFO("I'm Here2");
+            
+            robot_move = nodehandle_.advertise<geometry_msgs::PoseStamped>("/robot/moved", 1);
             
         }
 
@@ -49,45 +48,7 @@ class UrTest
             /* Empty */
         }
 
-        bool testMovePtp(const double q_change)
-        {
-            if (!doTestMovePtp(q_change))
-            {
-                std::cout<<"Check 3"<<std::endl;
-                return false;
-            }
-            ROS_INFO_STREAM("Waiting a little moment for the movement to be finished");
-            ros::Duration(5).sleep();  // In seconds
-
-            if (!doTestMovePtp(-q_change))
-            {
-                std::cout<<"Check 4"<<std::endl;
-                return false;
-            }
-            ROS_INFO_STREAM("Waiting a little moment for the movement to be finished");
-            ros::Duration(5).sleep();  // In seconds
-
-            return true;
-        }
-
-        bool testMoveServoQ(const double q_change)
-        {
-            if (!doTestMoveServoQ(q_change))
-            {
-                return false;
-            }
-            ROS_INFO_STREAM("Waiting a little moment for the movement to be finished");
-            ros::Duration(5).sleep();  // In seconds
-
-            if (!doTestMoveServoQ(-q_change))
-            {
-                return false;
-            }
-            ROS_INFO_STREAM("Waiting a little moment for the movement to be finished");
-            ros::Duration(5).sleep();  // In seconds
-
-            return true;
-        }
+       
         
 //         ros::std_msgs::Float64MultiArray get_current_Q()
         bool get_current_Q(mp_mini_picker::currentQ::Request  &req,
@@ -102,20 +63,6 @@ class UrTest
                     res.Q[i]=current_Q[i];
                 }
             }
-            
-            
-//             vector<double> vec1 = { 1.1, 2., 3.1};
-//             std_msgs::Float64MultiArray msg;
-// 
-//             // set up dimensions
-//             msg.layout.dim.push_back(std_msgs::MultiArrayDimension());
-//             msg.layout.dim[0].size = vec1.size();
-//             msg.layout.dim[0].stride = 1;
-//             msg.layout.dim[0].label = "x"; // or whatever name you typically use to index vec1
-// 
-//             // copy in the data
-//             msg.data.clear();
-//             msg.data.insert(msg.data.end(), vec1.begin(), vec1.end());
         }
         
         bool move_to_Q(mp_mini_picker::moveToQ::Request  &req,
@@ -144,6 +91,11 @@ class UrTest
             else
                 res.ok=1;
 //             cout << path.size() << endl;
+            
+            bool stop_succeeded = sdsip_.stop();
+            if(!stop_succeeded)
+                ROS_WARN("Did not succeed in sending stop command");
+            
             for (int i = 0; i<path.size() ; i++)
             {
                 rw::math::Q p = path[i];
@@ -158,32 +110,55 @@ class UrTest
 //                 
             }
             
-            rw::math::Q current_configuration = getCurrentJointConfiguration();
+//             rw::math::Q current_configuration = getCurrentJointConfiguration();
+//             rw::kinematics::State state = workcell_->getDefaultState();
+//             
+//             
+//             
+//             device_->setQ(current_configuration,state);
+// //             auto wTb=device_->worldTbase(state);
+//             
+// //             auto bTe=device_->baseTend(state);
+// //             auto tcp_frame=workcell_->findFrame("UR5.Joint5");
+//             auto tcp_frame=workcell_->findFrame("tool");
+//             auto bTe=device_->baseTframe(tcp_frame,state);
+//             auto bMarker= bTe*rw::math::Vector3D<double>(0,0,0);
+//             
+//             cout << bMarker << endl;
+//             
+//             geometry_msgs::Pose msg;
+//             msg.position.x= bMarker[0];
+//             msg.position.y= bMarker[1];
+//             msg.position.z= bMarker[2];
+// 
+//             robot_move.publish(msg);
+
+            
+        }
+        
+        void publish_position()
+        {
+            auto current_configuration=sdsip_.getQ();
             rw::kinematics::State state = workcell_->getDefaultState();
-            
-            
-            
             device_->setQ(current_configuration,state);
-//             auto wTb=device_->worldTbase(state);
+             auto wTb=device_->worldTbase(state);
             
 //             auto bTe=device_->baseTend(state);
 //             auto tcp_frame=workcell_->findFrame("UR5.Joint5");
             auto tcp_frame=workcell_->findFrame("tool");
             auto bTe=device_->baseTframe(tcp_frame,state);
-            auto bMarker= bTe*rw::math::Vector3D<double>(0,0,0);
+            auto bMarker= wTb*bTe*rw::math::Vector3D<double>(0,0,0);
             
-            cout << bMarker << endl;
+//             cout << bMarker << endl;
             
-            geometry_msgs::Pose msg;
-            msg.position.x= bMarker[0];
-            msg.position.y= bMarker[1];
-            msg.position.z= bMarker[2];
-
+            geometry_msgs::PoseStamped msg;
+            msg.pose.position.x= bMarker[0];
+            msg.pose.position.y= bMarker[1];
+            msg.pose.position.z= bMarker[2];
+            msg.header.stamp = ros::Time::now();
             robot_move.publish(msg);
 
-            
         }
-
     protected:
         
         void initWorkCell()
@@ -235,110 +210,29 @@ class UrTest
         rw::math::Q getCurrentJointConfiguration()
         {
             /* Make sure to get and operate on fresh data from the serial device
-                * It's assumed that the serial device is not moving
+                * It's assumed that the serial device is not movinghttps://e-learn.sdu.dk/bbcswebdav/pid-4929056-dt-content-rid-7854943_2/courses/EK-SSP-U1-1-E17/Agenda%20STATSIG%20lecture%2007.pdf
                 * ^- That could be asserted / verified using sdsip.isMoving()
                 * However other sources could invoke services on the UR that causes it to move...
                 */
-            ros::Time current_timestamp = ros::Time::now();
-            ros::Time obtained_timestamp = sdsip_.getTimeStamp();
-            while (current_timestamp > obtained_timestamp)
-            {
-                ros::Duration(0.1).sleep();  // In seconds
-                ros::spinOnce();
-                obtained_timestamp = sdsip_.getTimeStamp();
-            }
+//             ros::Time current_timestamp = ros::Time::now();
+//             ros::Time obtained_timestamp = sdsip_.getTimeStamp();
+//             ROS_INFO("Current timestamp: %s", current_timestamp);
+//             ROS_INFO("Obtained timestamp: %s", obtained_timestamp);
+//             if(!(sdsip_.isMoving())
+//             {
+//                 while (current_timestamp > obtained_timestamp )
+//                 {
+//                     ROS_INFO("Not getting fresh data from serial device - waiting 100 ms");
+//                     ros::Duration(0.1).sleep();  // In seconds
+//                     ros::spinOnce();
+//                     obtained_timestamp = sdsip_.getTimeStamp();
+//                 }
+//             }
 
             return sdsip_.getQ();
         }
 
-        rw::trajectory::QPath getQPath(const double q_change)
-        {
-            rw::math::Q start_configuration = getCurrentJointConfiguration();
-            rw::math::Q end_configuration = start_configuration + rw::math::Q(start_configuration.size(), q_change);
-            rw::trajectory::QPath path;
-            bool valid_path = false;
-            ROS_ASSERT(planner_);
-            valid_path = planner_->query(start_configuration, end_configuration, path);
-            if (!valid_path)
-            {
-                ROS_ERROR_STREAM("Could not find a path from '" << start_configuration << "' to '" << end_configuration << "'.");
-                throw std::runtime_error("No valid path found.");
-            }
-
-            return path;
-        }
-
-        rw::trajectory::QPath linearInterpolatedPath(const rw::math::Q& start, const rw::math::Q& end,
-                                                    const double total_duration = 10.0, const double duration_step = 1.0)
-        {
-            ROS_ASSERT(duration_step > 0);
-            ROS_ASSERT(duration_step < total_duration);
-
-            rw::trajectory::QLinearInterpolator interpolator(start, end, total_duration);
-
-            rw::trajectory::QPath path;
-
-            path.push_back(start);
-            for (double t = duration_step; t <= (total_duration - duration_step); t += duration_step)
-            {
-                path.push_back(interpolator.x(t));
-            }
-            path.push_back(end);
-
-            return path;
-        }
-
-        bool doTestMovePtp(const double q_change)
-        {
-            bool return_status = true;
-            std::cout<<"Check 5"<<std::endl;
-            cout << "no joke" << endl;
-            rw::trajectory::QPath path = getQPath(q_change);
-            std::cout<<"Check 6"<<std::endl;
-            for (const rw::math::Q& p : path)
-            {
-                ROS_INFO_STREAM("Ask to movePtp to '" << p << "'.");
-                bool ret = false;
-                std::cout<<"Check 7"<<std::endl;
-                ret = sdsip_.movePtp(p);
-                std::cout<<"Check 8"<<std::endl;
-                if (!ret)
-                {
-                std::cout<<"Check 9"<<std::endl;
-                return_status = false;
-                ROS_ERROR_STREAM("The serial device didn't acknowledge the movePtp command.");
-                }
-            }
-
-            return return_status;
-        }
-
-        bool doTestMoveServoQ(const double q_change)
-        {
-            bool return_status = true;
-            rw::trajectory::QPath path = getQPath(q_change);
-
-            ROS_ASSERT(path.size() == 2);
-            rw::math::Q start_configuration = path.at(0);
-            rw::math::Q end_configuration = path.at(1);
-
-            // replace the path with an interpolated path
-            path = linearInterpolatedPath(start_configuration, end_configuration);
-
-            for (const rw::math::Q& p : path)
-            {
-                ROS_INFO_STREAM("Ask to moveServoQ to '" << p << "'.");
-                bool ret = false;
-                ret = sdsip_.moveServoQ(p);
-                if (!ret)
-                {
-                return_status = false;
-                ROS_ERROR_STREAM("The serial device didn't acknowledge the moveServoQ command.");
-                }
-            }
-
-            return return_status;
-        }
+        
 
     protected:
         ros::NodeHandle nodehandle_;
@@ -362,26 +256,29 @@ int main(int argc, char* argv[])
     UrTest ur_test;
     
     
-    
-//     bool ret = false;
-//     //   ret = ur_test.testMovePtp(q_change);
-// 
-//     if (!ret)
-//     {
-//         ROS_ERROR_STREAM("Could not properly do the testMovePtp");
-//         return 1;
-//     }
-    
-    
-    //ros::Publisher current_Q_pub = n.advertise<std_msgs::Float64MultiArray>("current_Q", 1000);
-    
-    ros::ServiceServer service = n.advertiseService("GetCurrentQ", &UrTest::get_current_Q, &ur_test);
-    ros::ServiceServer service2 = n.advertiseService("MoveToQ", &UrTest::move_to_Q, &ur_test);
+    ros::ServiceServer service = n.advertiseService("/robot/GetCurrentQ", &UrTest::get_current_Q, &ur_test);
+    ros::ServiceServer service2 = n.advertiseService("/robot/MoveToQ", &UrTest::move_to_Q, &ur_test);
     
     
     
     ROS_INFO("We are about to spin");
-    ros::spin();
+    //ros::spin();
+    ros::Time last_publish=ros::Time::now();
+    ros::Duration time_between_publishing(0.14285); // camera has framerate of 7 hz
+    ros::Duration time_difference;
+    while(true)
+    {
+        ros::Time current_time=ros::Time::now();
+        time_difference=current_time-last_publish;
+        if(time_difference>=time_between_publishing)
+        {
+//             ROS_INFO("I'm inside");
+            last_publish=ros::Time::now();
+            ur_test.publish_position();
+        }
+        
+        ros::spinOnce();
+    }
 
     return 0;
 }
